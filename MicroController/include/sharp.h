de88d6e1f095aa50b7b3beeff5fc117c03e311dd
Gavin <23hdh1@queensu.ca>
2026-03-18 22:45:59 -0400

    ssss

--- MicroController/include/sharp.h
+++ MicroController/include/sharp.h
@@ -1,26 +1,26 @@
-//sharp.h
+// //sharp.h
 
-#ifndef SHARP_H
-#define SHARP_H
+// #ifndef SHARP_H
+// #define SHARP_H
 
-#include <Arduino.h>
+// #include <Arduino.h>
 
-class Sharp {
-public:
+// class Sharp {
+// public:
 
-int front_sharp_val = 0;   
-int right_sharp_val = 0;
-int left_sharp_val = 0;
+// int front_sharp_val = 0;   
+// int right_sharp_val = 0;
+// int left_sharp_val = 0;
 
-void read();
-void print();
+// void read();
+// void print();
    
-private:
+// private:
   
-const byte FRONT_SHARP_PIN = A2;
-const byte RIGHT_SHARP_PIN = A1;
-const byte LEFT_SHARP_PIN = A0;
+// const byte FRONT_SHARP_PIN = A2;
+// const byte RIGHT_SHARP_PIN = A1;
+// const byte LEFT_SHARP_PIN = A0;
 
-};
+// };
 
-#endif // SHARP_H
+// #endif // SHARP_H
